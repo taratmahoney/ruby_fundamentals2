086d42832f5cf2857a2bd86f626cd0f34709904d
Tara Mahoney <tara.mahoney@me.com>
2015-04-01 15:40:22 -0400

    Printed a grocery list with each item on it's own line, added rice, includeda statement about bananas if bananas were on the grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -3,3 +3,21 @@ grocery_list = ["bananas", "watercress", "apples", "oranges"]
 grocery_list.each do |item|
 	puts "* #{item}"
 end
+
+grocery_list.push("rice")
+
+def print_groceries(grocery_list)
+	grocery_list.each do |item|
+	puts "* #{item}"
+	end
+end
+
+print_groceries(grocery_list)
+
+grocery_list.include?("bananas")
+
+if grocery_list.include? ("bananas")
+	puts"You need to pick up bananas"
+else
+	puts "You don't need to pick up bananas"
+end
